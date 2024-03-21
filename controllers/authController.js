4f9a0ec6dfbebf91ae59363ec8e80e0d8cb3e718
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-03-21 19:45:43 +0530

    renamed files

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const otpGenerator = require("otp-generator");
 const crypto = require("crypto");
 
-const mailService = require("../services/mailer");
+// const mailService = require("../services/mailer");
 
 const User = require("../models/user");
 const filterObj = require("../utils/filterObj");
@@ -28,132 +28,153 @@ exports.login = async (req, res, next) => {
 
   const userDoc = await User.findOne({ email: email }).select("+password");
 
-  if (!user || !(await user.correctPassword(password, userDoc.password))) {
+  if (
+    !userDoc ||
+    !(await userDoc.correctPassword(password, userDoc.password))
+  ) {
     res.status(400).json({
       status: "error",
       message: "Email or Password is incorrect",
     });
+    return;
   }
 
   const token = signToken(userDoc._id);
 
-  // Signup => register - sendOTP - verifyOTP
+  res.status(200).json({
+    stauts: "success",
+    message: "Logged in Successfully!",
+    token,
+    user_id: userDoc._id,
+  });
+};
+// Signup => register - sendOTP - verifyOTP
 
-  // https://api.tawk.com/auth/register
+// https://api.tawk.com/auth/register
 
-  //Register New User
-  exports.register = async (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body;
+//Register New User
+exports.register = async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
 
-    const filteredBody = filterObj(
-      req.body,
-      "firstName",
-      "lastName",
-      "password",
-      "email",
-    );
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "password",
+    "email",
+  );
 
-    // check if a verified user with given email exists
+  // check if a verified user with given email exists
 
-    const existing_user = await User.findOne({ email: email });
+  const existing_user = await User.findOne({ email: email });
 
-    if (existing_user && existing_user.verified) {
-      res.status(400).json({
-        status: "error",
-        message: "Email is already in use, Please login",
-      });
-    } else if (existing_user) {
-      await User.findOneAndUpdate({ email: email }, filteredBody, {
-        new: true,
-        validateModifiedOnly: true,
-      });
-
-      req.userId = existing_user._id;
-      next();
-    } else {
-      // if user record is not available in DB
-      const new_user = await User.create(filteredBody);
-
-      //generate OTP and send email to user
-      req.userId = new_user._id;
-
-      next();
-    }
-  };
-
-  exports.sendOTP = async () => {
-    const { userId } = req;
-    const new_otp = otpGenerator.generate(6, {
-      upperCaseAlphabets: false,
-      lowerCaseAlphabets: false,
-      specialChars: false,
+  if (existing_user && existing_user.verified) {
+    res.status(400).json({
+      status: "error",
+      message: "Email is already in use, Please login",
     });
-
-    const otp_expiry_time = Date.now() + 10 * 60 * 100; // 10 minutes
-
-    await User.findByIdAndUpdate(userId, {
-      otp: new_otp,
-      otp_expiry_time,
-    });
-
-    // TODO Send Mail
-    mailService
-      .sendEmail({
-        from: "rawatdevanshu22@gmail.com",
-        to: "example@gmail.com",
-        subject: "OTP for Tawk",
-        text: `Your otp is ${new_otp}. this is valid for 10 minutes`,
-      })
-      .then(() => {})
-      .catch((err) => {});
-
-    res.status(200).json({
-      status: "success",
-      message: "OTP Sent Successfully",
-    });
-  };
-
-  exports.verifyOTP = async (req, res, next) => {
-    //verifyOTP and update user record accordingly
-
-    const { email, otp } = req.body;
-
-    const userDoc = await User.findOne({
-      email,
-      otp_expiry_time: { $gt: Date.now() },
-    });
-
-    if (!userDoc) {
-      res.status(400).json({
-        status: "error",
-        message: "Email is Invalid or OTP expired",
-      });
-    }
-
-    if (!(await userDoc.correctOTP(otp, userDoc.otp))) {
-      res.status(400).json({
-        status: "error",
-        message: "OTP is incorrect",
-      });
-    }
-
-    // OTP is correct
-    userDoc.verified = true;
-    userDoc.otp = undefined;
-
-    await userDoc.save({
+  } else if (existing_user) {
+    await User.findOneAndUpdate({ email: email }, filteredBody, {
       new: true,
       validateModifiedOnly: true,
     });
 
-    const token = signToken(userDoc._id);
+    req.userId = existing_user._id;
+    next();
+  } else {
+    // if user record is not available in DB
+    const new_user = await User.create(filteredBody);
 
-    res.status(200).json({
-      status: "success",
-      message: "OTP verified successfully",
-      token,
+    //generate OTP and send email to user
+    req.userId = new_user._id;
+    next();
+  }
+};
+
+exports.sendOTP = async (req, res, next) => {
+  const { userId } = req;
+  const new_otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+  });
+
+  const otp_expiry_time = Date.now() + 10 * 60 * 1000; // 10 minutes
+  const userDoc = await User.findByIdAndUpdate(userId, {
+    otp: new_otp,
+    otp_expiry_time,
+  });
+
+  userDoc.otp = new_otp;
+
+  await userDoc.save({ new: true, validateModifiedOnly: true });
+
+  // TODO Send Mail
+  // mailService
+  //   .sendEmail({
+  //     from: "rawatdevanshu22@gmail.com",
+  //     to: "example@gmail.com",
+  //     subject: "OTP for Tawk",
+  //     text: `Your otp is ${new_otp}. this is valid for 10 minutes`,
+  //   })
+  //   .then(() => {})
+  //   .catch((err) => {});
+
+  res.status(200).json({
+    status: "success",
+    message: "OTP Sent Successfully",
+  });
+};
+
+exports.verifyOTP = async (req, res, next) => {
+  //verifyOTP and update user record accordingly
+
+  const { email, otp } = req.body;
+
+  const userDoc = await User.findOne({
+    email,
+    otp_expiry_time: { $gt: Date.now() },
+  });
+
+  if (!userDoc) {
+    res.status(400).json({
+      status: "error",
+      message: "Email is Invalid or OTP expired",
     });
-  };
+    return;
+  }
+
+  if (userDoc.verified) {
+    return res.status(400).json({
+      status: "error",
+      message: "Email is already verified",
+    });
+  }
+
+  if (!(await userDoc.correctOTP(otp, userDoc.otp))) {
+    res.status(400).json({
+      status: "error",
+      message: "OTP is incorrect",
+    });
+    return;
+  }
+
+  // OTP is correct
+  userDoc.verified = true;
+  userDoc.otp = undefined;
+
+  await userDoc.save({
+    new: true,
+    validateModifiedOnly: true,
+  });
+
+  const token = signToken(userDoc._id);
+
+  res.status(200).json({
+    status: "success",
+    message: "OTP verified successfully",
+    token,
+  });
 };
 
 exports.protect = async (req, res, next) => {
@@ -214,14 +235,18 @@ exports.forgotPassword = async (req, res, next) => {
   }
 
   // 2) generate random reset token
-  const resetToken = user.createPasswordResetToke();
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
   const resetURL = `https://tawk.com/auth/reset-password/?code=${resetToken}`;
   try {
     // TODO => Send Emai With Reset URL
+    console.log(resetToken);
     res.status(200).json({
       status: "success",
       message: "Reset Password link sent to Email",
     });
+    return;
   } catch (error) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
@@ -239,7 +264,7 @@ exports.resetPassword = async (req, res, next) => {
   // 1) get user based on token
   const hashedToken = crypto
     .createHash("sha256")
-    .update(req.params.token)
+    .update(req.body.token)
     .digest("hex");
 
   const user = await User.findOne({
@@ -253,6 +278,7 @@ exports.resetPassword = async (req, res, next) => {
       status: "error",
       message: "Token is Invalid or Expired",
     });
+    return;
   }
 
   // 3) Updated users password and set resetToken and expiry to undefined
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const authController = require("../controllers/auth");
+const authController = require("../controllers/authController");
 
 router.post("/login", authController.login);
-router.post("/register", authController.register);
+router.post("/register", authController.register, authController.sendOTP);
 router.post("/send-otp", authController.sendOTP);
 router.post("/verify-otp", authController.verifyOTP);
 router.post("/forgot-password", authController.forgotPassword);
